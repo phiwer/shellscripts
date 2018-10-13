7017dbe1cd2c2448764f352f51eb49e2b785b694
Philip Werner <phiwer@gmail.com>
2018-10-13 21:15:38 +0200

    Fix path to wallpaper

--- screenlayout/set-screen-layout.sh
+++ screenlayout/set-screen-layout.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-#ROOT_DOTFILES_DIR="$(dirname "$SCRIPT_DIR")"
-
-#$SCRIPT_DIR/virtual-dell.sh
-
-feh --bg-scale "$WALLPAPERS/1984.jpg"
+feh --bg-scale "$HOME/.wallpapers/1984.jpg"&
