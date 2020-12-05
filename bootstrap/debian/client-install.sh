f56c542fc6fe9228b7bd1ebd8adc053808e73e95
Philip Werner <phiwer@gmail.com>
2020-12-05 22:48:36 +0100

    Fix bugs

--- bootstrap/debian/client-install.sh
+++ bootstrap/debian/client-install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+## TODO: Add non-free and contrib to sources.list
+
 set -e
 
 if [[ $EUID -ne 0 ]]; then
@@ -19,26 +21,29 @@ sudo -u $SUDO_USER mkdir -p "$BIN"
 
 # Add keys
 
+## Multimedia
+#wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb && sudo dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
+
 ## Spotify
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D1742AD60D811D58
+echo "deb [allow-insecure=yes trusted=yes arch=amd64] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 ## Chrome
-
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
 ## Docker
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-echo "deb [allow-insecure=yes arch=amd64] https://download.docker.com/linux/debian buster stable" | sudo tee /etc/apt/sources.list.d/docker-stable.list
+echo "deb [allow-insecure=yes trusted=yes arch=amd64] https://download.docker.com/linux/debian buster stable" | sudo tee /etc/apt/sources.list.d/docker-stable.list
 
-# Add FS-UAE repository
-echo "deb [allow-insecure=yes] http://download.opensuse.org/repositories/home:/FrodeSolheim:/stable/Debian_9.0/ /" | sudo tee /etc/apt/sources.list.d/FrodeSolheim-stable.list
-wget -q -O - http://download.opensuse.org/repositories/home:FrodeSolheim:stable/Debian_9.0/Release.key | sudo apt-key add -
+# Add FS-UAE repository1
+wget -q -O - https://download.opensuse.org/repositories/home:FrodeSolheim:stable/Debian_10/Release.key | sudo apt-key add -
+echo "deb [allow-insecure=yes trusted=yes] https://download.opensuse.org/repositories/home:/FrodeSolheim:/stable/Debian_10/ /" | sudo tee /etc/apt/sources.list.d/FrodeSolheim-stable.list
 
 
 # Update repositories and update
-
 apt update --allow-unauthenticated
 
 apt autoremove
@@ -64,6 +69,7 @@ apt -qq install -y \
     cpufrequtils \
     curl \
     default-jdk \
+    dh-autoreconf \
     dirmngr \
     docker-ce \
     emacs \
@@ -84,6 +90,7 @@ apt -qq install -y \
     imagemagick \
     irssi \
     libssl1.1 \
+    libjsoncpp-dev \
     libsecret-1-0 \
     libsecret-1-dev \
     libxcb-keysyms1-dev \
@@ -115,10 +122,35 @@ apt -qq install -y \
     libxcb1-dev \
     libxcb-image0-dev \
     libxcb-ewmh-dev \
+    libboost-all-dev \
     lightdm \
     linux-perf \
+    libxcb-keysyms1-dev \
+    libpango1.0-dev \
+    libxcb-util0-dev \
+    xcb \
+    libxcb1-dev \
+    libxcb-icccm4-dev \
+    libyajl-dev \
+    libev-dev \
+    libxcb-xkb-dev \
+    libxcb-cursor-dev \
+    libxkbcommon-dev \
+    libxcb-xinerama0-dev \
+    libxkbcommon-x11-dev \
+    libstartup-notification0-dev \
+    libxcb-randr0-dev \
+    libxcb-xrm0 \
+    libxcb-xrm-dev \
+    libxcb-shape0 \
+    libxcb-shape0-dev \
+    libxcb1-dev \
+    libxcb-util0-dev \
+    libxcb-randr0-dev \
+    libxcb-composite0-dev \
     mc \
     meld \
+    meson \
     minicom \
     nvidia-settings \
     nvme-cli \
@@ -131,7 +163,6 @@ apt -qq install -y \
     python3-pip \
     python3-xcbgen \
     qtcreator \
-    qt5-default \
     read-edid \
     rofi \
     rxvt-unicode \
@@ -180,16 +211,21 @@ else
 fi
 
 # compile & install
-sudo -u $SUDO_USER autoreconf --force --install
-sudo -u $SUDO_USER rm -rf build/
-sudo -u $SUDO_USER mkdir -p build
-cd build/
+sudo -u $SUDO_USER mkdir -p build && cd build
+sudo -u $SUDO_USER meson ..
+sudo -u $SUDO_USER ninja
+meson install
+
+#sudo -u $SUDO_USER autoreconf --force --install
+#sudo -u $SUDO_USER rm -rf build/
+#sudo -u $SUDO_USER mkdir -p build
+#cd build/
 
 # Disabling sanitizers is important for release versions!
 # The prefix and sysconfdir are, obviously, dependent on the distribution.
-sudo -u $SUDO_USER ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-sudo -u $SUDO_USER make
-sudo make install
+#sudo -u $SUDO_USER ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+#sudo -u $SUDO_USER make
+#sudo make install
 
 ## Powerline fonts
 if [ -d "$EXTERNAL/powerline-fonts" ]; then
@@ -203,14 +239,14 @@ sudo -u $SUDO_USER ./install.sh
 
 ## Polybar
 
-POLYBAR_FILE="polybar-3.4.3.tar"
+POLYBAR_FILE="polybar-3.5.0.tar"
 sudo -u $SUDO_USER mkdir -p "downloads"
-sudo -u $SUDO_USER wget "https://github.com/jaagr/polybar/releases/download/3.4.3/$POLYBAR_FILE" -P downloads/
+sudo -u $SUDO_USER wget "https://github.com/jaagr/polybar/releases/download/3.5.0/$POLYBAR_FILE" -P downloads/
 sudo -u $SUDO_USER tar xvf "downloads/$POLYBAR_FILE" -C "$EXTERNAL/"
 
 cd "$EXTERNAL/polybar"
 
-sudo -u $SUDO_USER rm -rf build
+rm -rf build
 sudo -u $SUDO_USER mkdir build
 cd build
 sudo -u $SUDO_USER cmake ..
